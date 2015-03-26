468f8c26b757fb1a93335436952b9068f3b1cce0
Hermann Ziak <hermann.ziak@gmail.com>
2015-03-26 09:13:15 +0100

    removed old files

--- code/experimental/misc/src/main/java/eu/eexcess/diversityeasurement/ndcg/NDCGResultList.java
+++
@@ -1,9 +0,0 @@
-package eu.eexcess.diversityeasurement.ndcg;
-import java.util.List;
-
-import eu.eexcess.dataformats.result.ResultList;
-
-
-public class NDCGResultList {
-	 List<NDCGResult> results;
-}
