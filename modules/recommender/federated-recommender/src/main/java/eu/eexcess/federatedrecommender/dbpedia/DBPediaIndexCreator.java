982ff9280fc4bb17fa34249502eca754890b8ae6
hziak <hermann@ziak.biz>
2015-07-03 09:58:44 +0200

    minor fixes

--- modules/recommender/federated-recommender/src/main/java/eu/eexcess/federatedrecommender/dbpedia/DBPediaIndexCreator.java
+++ modules/recommender/federated-recommender/src/main/java/eu/eexcess/federatedrecommender/dbpedia/DBPediaIndexCreator.java
@@ -19,7 +19,7 @@ GNU Affero General Public License for more details.
 
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
 package eu.eexcess.federatedrecommender.dbpedia;
 
 import java.util.Arrays;
@@ -27,83 +27,90 @@ import java.util.List;
 
 import eu.eexcess.config.FederatedRecommenderConfiguration;
 import eu.eexcess.federatedrecommender.utils.FederatedRecommenderException;
+
 /**
- * Class to create the solrIndex with the given Dbpedia mapping files at the given solr index uri
+ * Class to create the solrIndex with the given Dbpedia mapping files at the
+ * given solr index uri
+ * 
  * @author hziak
  *
  */
 public class DBPediaIndexCreator {
 
-	public static void main(String[] args) {
-		if (args.length == 0) {
-			String message = UsageMessage();
-			System.out.println(message);
-			return;
-		}
-		int solrServerUriIndex=-1;
-		int dbPediaBaseFilesIndex=-1;
-		for (int i = 0; i < args.length; i++) {
-			if(args[i].equals("-s"))
-				 solrServerUriIndex =i;
-			else if(args[i].equals("-i"))
-				dbPediaBaseFilesIndex=i;
-		}
-		
-		
-		String solrServerUri=null;
-		if(solrServerUriIndex>=0)
-			if(solrServerUriIndex+1< args.length)
-				solrServerUri = args[solrServerUriIndex+1];
-		if(solrServerUri==null){
-			String message =  "Solr server uri not found \n " +UsageMessage();
-			System.out.println(message);
-			return;
-		}
-		FederatedRecommenderConfiguration configuration = new FederatedRecommenderConfiguration();
-		configuration.solrServerUri =solrServerUri;
-		List<String> dbPediaFileList=null;
-		if(dbPediaBaseFilesIndex>=0)
-			if(dbPediaBaseFilesIndex+1< args.length){
-				if(dbPediaBaseFilesIndex< solrServerUriIndex)
-					dbPediaFileList =Arrays.asList(args).subList(dbPediaBaseFilesIndex+1, solrServerUriIndex);
-				else
-					dbPediaFileList =Arrays.asList(args).subList(dbPediaBaseFilesIndex+1, args.length);
-			}
-			
-			DbPediaSolrIndex dbPediaSolrIndex = new DbPediaSolrIndex(configuration);
-			if(dbPediaFileList!=null){
-				
-				
-				for (int j = 0; j < dbPediaFileList.size(); j++) {
-					try {
-						dbPediaSolrIndex.createIndex(dbPediaFileList.get(j),j*30000000, true); /* 40000000 to not override old entry's with the same id!*/
-					} catch (FederatedRecommenderException e) {
-						e.printStackTrace();
-					}
-				}
-				
-			}
-			else {
-				System.out.println("No files to index, just doing reference counting");
-			}
-		
-	
-		try {
-				dbPediaSolrIndex.createReferenceCounts();
-			} catch (FederatedRecommenderException e) {
-				e.printStackTrace();
-			}
-		
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            String message = UsageMessage();
+            System.out.println(message);
+            return;
+        }
+        int solrServerUriIndex = -1;
+        int dbPediaBaseFilesIndex = -1;
+        for (int i = 0; i < args.length; i++) {
+            if ("-s".equals(args[i]))
+                solrServerUriIndex = i;
+            else if ("-i".equals(args[i]))
+                dbPediaBaseFilesIndex = i;
+        }
 
-	}
+        String solrServerUri = null;
+        if (solrServerUriIndex >= 0 && solrServerUriIndex + 1 < args.length)
+            solrServerUri = args[solrServerUriIndex + 1];
+        if (solrServerUri == null) {
+            String message = "Solr server uri not found \n " + UsageMessage();
+            System.out.println(message);
+            return;
+        }
+        FederatedRecommenderConfiguration configuration = new FederatedRecommenderConfiguration();
+        configuration.solrServerUri = solrServerUri;
+        List<String> dbPediaFileList = null;
+        if (dbPediaBaseFilesIndex >= 0 & dbPediaBaseFilesIndex + 1 < args.length) {
+            if (dbPediaBaseFilesIndex < solrServerUriIndex)
+                dbPediaFileList = Arrays.asList(args).subList(dbPediaBaseFilesIndex + 1, solrServerUriIndex);
+            else
+                dbPediaFileList = Arrays.asList(args).subList(dbPediaBaseFilesIndex + 1, args.length);
+        }
 
-	private static String UsageMessage() {
-		String message = "Usage: \n"
-				+ "-s <SolrServerUri>   (e.g. http://localhost:8983/solr/)\n"
-				+ "-i <DbpediaMappingBasedFile i> .... <DbpediaMappingBasedFile n>\n"
-				+ "Note: For example use the Dbpedia files named mappingbased_properties_en.nt or mappingbased_properties_en_uris_de.nt \n"
-				+ "mappingbased_properties_en.nt should always be used as basefile and first file in the index.\n"
-				+ "For other languages then english and german adaptation of the SolrSchema should be done";
-		return message;
-	}
+        DbPediaSolrIndex dbPediaSolrIndex = new DbPediaSolrIndex(configuration);
+        if (dbPediaFileList != null) {
+
+            for (int j = 0; j < dbPediaFileList.size(); j++) {
+                try {
+                    dbPediaSolrIndex.createIndex(dbPediaFileList.get(j), j * 30000000, true); /*
+                                                                                               * 40000000
+                                                                                               * to
+                                                                                               * not
+                                                                                               * override
+                                                                                               * old
+                                                                                               * entry
+                                                                                               * 's
+                                                                                               * with
+                                                                                               * the
+                                                                                               * same
+                                                                                               * id
+                                                                                               * !
+                                                                                               */
+                } catch (FederatedRecommenderException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        } else {
+            System.out.println("No files to index, just doing reference counting");
+        }
+
+        try {
+            dbPediaSolrIndex.createReferenceCounts();
+        } catch (FederatedRecommenderException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static String UsageMessage() {
+        String message = "Usage: \n" + "-s <SolrServerUri>   (e.g. http://localhost:8983/solr/)\n" + "-i <DbpediaMappingBasedFile i> .... <DbpediaMappingBasedFile n>\n"
+                + "Note: For example use the Dbpedia files named mappingbased_properties_en.nt or mappingbased_properties_en_uris_de.nt \n"
+                + "mappingbased_properties_en.nt should always be used as basefile and first file in the index.\n"
+                + "For other languages then english and german adaptation of the SolrSchema should be done";
+        return message;
+    }
 }
