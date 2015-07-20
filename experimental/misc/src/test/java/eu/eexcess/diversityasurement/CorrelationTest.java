d1d808aaffefdc120368696c34f4e6e093622758
Raoul Rubien <raoul.rubien@know-center.at>
2015-07-20 11:27:52 +0200

    refined .gitignore

--- experimental/misc/src/test/java/eu/eexcess/diversityasurement/CorrelationTest.java
+++ experimental/misc/src/test/java/eu/eexcess/diversityasurement/CorrelationTest.java
@@ -30,108 +30,107 @@ import org.junit.Test;
 
 public class CorrelationTest {
 
-	private static class CorrelationValues {
-		public double[] x;
-		public double[] y;
-		public double spearmanRho;
-		public double kendallTaoB;
-		public double epsilon = 0.01;
-	}
+    private static class CorrelationValues {
+        public double[] x;
+        public double[] y;
+        public double spearmanRho;
+        public double kendallTaoB;
+        public double epsilon = 0.01;
+    }
 
-	private CorrelationValues newCorrelationValue1() {
-		CorrelationValues c = new CorrelationValues();
-		c.x = new double[] { 56, 75, 45, 71, 61, 64, 58, 80, 76, 61 };
-		c.y = new double[] { 66, 70, 40, 60, 65, 56, 59, 77, 67, 63 };
-		c.spearmanRho = 0.67;
-		c.kendallTaoB = 0.49;
-		return c;
-	}
+    private CorrelationValues newCorrelationValue1() {
+        CorrelationValues c = new CorrelationValues();
+        c.x = new double[] { 56, 75, 45, 71, 61, 64, 58, 80, 76, 61 };
+        c.y = new double[] { 66, 70, 40, 60, 65, 56, 59, 77, 67, 63 };
+        c.spearmanRho = 0.67;
+        c.kendallTaoB = 0.49;
+        return c;
+    }
 
-	private CorrelationValues newCorrelationValue2() {
-		CorrelationValues c = new CorrelationValues();
-		c.x = new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-		c.y = new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-		c.spearmanRho = 1;
-		c.kendallTaoB = 1;
-		return c;
-	}
+    private CorrelationValues newCorrelationValue2() {
+        CorrelationValues c = new CorrelationValues();
+        c.x = new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        c.y = new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        c.spearmanRho = 1;
+        c.kendallTaoB = 1;
+        return c;
+    }
 
-	private CorrelationValues newCorrelationValue3() {
-		CorrelationValues c = new CorrelationValues();
-		c.x = new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-		c.y = new double[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
-		c.spearmanRho = -1;
-		c.kendallTaoB = -1;
-		return c;
-	}
+    private CorrelationValues newCorrelationValue3() {
+        CorrelationValues c = new CorrelationValues();
+        c.x = new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        c.y = new double[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+        c.spearmanRho = -1;
+        c.kendallTaoB = -1;
+        return c;
+    }
 
-	private CorrelationValues newCorrelationValue4() {
-		CorrelationValues c = new CorrelationValues();
-		c.x = new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-		c.y = new double[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-		c.spearmanRho = 0;
-		c.kendallTaoB = 1;
-		return c;
-	}
+    private CorrelationValues newCorrelationValue4() {
+        CorrelationValues c = new CorrelationValues();
+        c.x = new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        c.y = new double[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        c.spearmanRho = 0;
+        c.kendallTaoB = 1;
+        return c;
+    }
 
-	@Test
-	public void spearmansRho_givenDefaultValues_expectCorrectResult() {
-		CorrelationValues c = newCorrelationValue1();
-		SpearmansCorrelation sCorrelation = new SpearmansCorrelation();
-		PearsonsCorrelation pCorrelatoin = new PearsonsCorrelation();
+    @Test
+    public void spearmansRho_givenDefaultValues_expectCorrectResult() {
+        CorrelationValues c = newCorrelationValue1();
+        SpearmansCorrelation sCorrelation = new SpearmansCorrelation();
+        PearsonsCorrelation pCorrelatoin = new PearsonsCorrelation();
 
-		System.out.println("spearman correlation r(x, y)=" + sCorrelation.correlation(c.x, c.y));
-		assertEquals(sCorrelation.correlation(c.x, c.y), c.spearmanRho, c.epsilon);
-		System.out.println("pearson correlation  r(x, y)=" + pCorrelatoin.correlation(c.x, c.y));
-		assertEquals(sCorrelation.correlation(c.y, c.x), c.spearmanRho, c.epsilon);
-	}
+        System.out.println("spearman correlation r(x, y)=" + sCorrelation.correlation(c.x, c.y));
+        assertEquals(sCorrelation.correlation(c.x, c.y), c.spearmanRho, c.epsilon);
+        System.out.println("pearson correlation  r(x, y)=" + pCorrelatoin.correlation(c.x, c.y));
+        assertEquals(sCorrelation.correlation(c.y, c.x), c.spearmanRho, c.epsilon);
+    }
 
-	@Test
-	public void spearmansRho_givenAKartesianQuadrandIIandIVSymmetralePrallelLine() {
-		CorrelationValues c = newCorrelationValue2();
-		SpearmansCorrelation sCorrelation = new SpearmansCorrelation();
-		System.out.println("spearman correlation r(x, y)=" + sCorrelation.correlation(c.x, c.y));
-		assertEquals(sCorrelation.correlation(c.x, c.y), c.spearmanRho, c.epsilon);
-	}
+    @Test
+    public void spearmansRho_givenAKartesianQuadrandIIandIVSymmetralePrallelLine() {
+        CorrelationValues c = newCorrelationValue2();
+        SpearmansCorrelation sCorrelation = new SpearmansCorrelation();
+        System.out.println("spearman correlation r(x, y)=" + sCorrelation.correlation(c.x, c.y));
+        assertEquals(sCorrelation.correlation(c.x, c.y), c.spearmanRho, c.epsilon);
+    }
 
-	@Test
-	public void spearmansRho_givenAKartesianQuadrandIandIIISymmetraleParallelLine() {
-		CorrelationValues c = newCorrelationValue3();
-		SpearmansCorrelation sCorrelation = new SpearmansCorrelation();
-		System.out.println("spearman correlation r(x, y)=" + sCorrelation.correlation(c.x, c.y));
-		assertEquals(sCorrelation.correlation(c.x, c.y), c.spearmanRho, c.epsilon);
-	}
+    @Test
+    public void spearmansRho_givenAKartesianQuadrandIandIIISymmetraleParallelLine() {
+        CorrelationValues c = newCorrelationValue3();
+        SpearmansCorrelation sCorrelation = new SpearmansCorrelation();
+        System.out.println("spearman correlation r(x, y)=" + sCorrelation.correlation(c.x, c.y));
+        assertEquals(sCorrelation.correlation(c.x, c.y), c.spearmanRho, c.epsilon);
+    }
 
-	@Test
-	public void spearmansRho_givenPosXAxis() {
-		CorrelationValues c = newCorrelationValue4();
-		SpearmansCorrelation sCorrelation = new SpearmansCorrelation();
-		System.out.println("spearman correlation r(x, y)=" + sCorrelation.correlation(c.x, c.y));
-		assertTrue(Double.isNaN(sCorrelation.correlation(c.x, c.y)));
-	}
+    @Test
+    public void spearmansRho_givenPosXAxis() {
+        CorrelationValues c = newCorrelationValue4();
+        SpearmansCorrelation sCorrelation = new SpearmansCorrelation();
+        System.out.println("spearman correlation r(x, y)=" + sCorrelation.correlation(c.x, c.y));
+        assertTrue(Double.isNaN(sCorrelation.correlation(c.x, c.y)));
+    }
 
-	@Test
-	public void kendallsTao_givenAKartesianQuadrandIandIIISymmetraleParallelLine() {
-		CorrelationValues c = newCorrelationValue3();
-		KendallsCorrelation kCorrelation = new KendallsCorrelation();
-		System.out.println("kendall's tao correlation t(x, y)=" + kCorrelation.correlation(c.x, c.y));
-		assertEquals(c.kendallTaoB, kCorrelation.correlation(c.x, c.y), c.epsilon);
-	}
+    @Test
+    public void kendallsTao_givenAKartesianQuadrandIandIIISymmetraleParallelLine() {
+        CorrelationValues c = newCorrelationValue3();
+        KendallsCorrelation kCorrelation = new KendallsCorrelation();
+        System.out.println("kendall's tao correlation t(x, y)=" + kCorrelation.correlation(c.x, c.y));
+        assertEquals(c.kendallTaoB, kCorrelation.correlation(c.x, c.y), c.epsilon);
+    }
 
-	
-	@Test
-	public void kendallsTao_givenAKartesianQuadrandIIandIVSymmetralePrallelLine() {
-		CorrelationValues c = newCorrelationValue2();
-		KendallsCorrelation kCorrelation = new KendallsCorrelation();
-		System.out.println("kendall's tao correlation t(x, y)=" + kCorrelation.correlation(c.x, c.y));
-		assertEquals(c.kendallTaoB, kCorrelation.correlation(c.x, c.y), c.epsilon);
-	}
-	
-	@Test
-	public void kendallsTao_givenDefaultValues_expectCorrectResult() {
-		CorrelationValues c = newCorrelationValue1();
-		KendallsCorrelation kCorrelation = new KendallsCorrelation();
-		System.out.println("kendall's tao correlation t(x, y)=" + kCorrelation.correlation(c.x, c.y));
-		assertEquals(c.kendallTaoB, kCorrelation.correlation(c.x, c.y), c.epsilon);
-	}
+    @Test
+    public void kendallsTao_givenAKartesianQuadrandIIandIVSymmetralePrallelLine() {
+        CorrelationValues c = newCorrelationValue2();
+        KendallsCorrelation kCorrelation = new KendallsCorrelation();
+        System.out.println("kendall's tao correlation t(x, y)=" + kCorrelation.correlation(c.x, c.y));
+        assertEquals(c.kendallTaoB, kCorrelation.correlation(c.x, c.y), c.epsilon);
+    }
+
+    @Test
+    public void kendallsTao_givenDefaultValues_expectCorrectResult() {
+        CorrelationValues c = newCorrelationValue1();
+        KendallsCorrelation kCorrelation = new KendallsCorrelation();
+        System.out.println("kendall's tao correlation t(x, y)=" + kCorrelation.correlation(c.x, c.y));
+        assertEquals(c.kendallTaoB, kCorrelation.correlation(c.x, c.y), c.epsilon);
+    }
 }
