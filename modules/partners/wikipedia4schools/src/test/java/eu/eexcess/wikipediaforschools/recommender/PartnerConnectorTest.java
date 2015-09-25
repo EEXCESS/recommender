67b339731a488c25feca54cf4a96d7ffbc9bf551
hziak <hermann@ziak.biz>
2015-09-25 10:07:13 +0200

    wikipedia for schools index intregration

---
+++ modules/partners/wikipedia4schools/src/test/java/eu/eexcess/wikipediaforschools/recommender/PartnerConnectorTest.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (C) 2015
+ * "Kompetenzzentrum fuer wissensbasierte Anwendungen Forschungs- und EntwicklungsgmbH" 
+ * (Know-Center), Graz, Austria, office@know-center.at.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author Raoul Rubien
+ */
+
+package eu.eexcess.wikipediaforschools.recommender;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import eu.eexcess.config.PartnerConfiguration;
+import eu.eexcess.dataformats.result.ResultList;
+import eu.eexcess.dataformats.userprofile.ContextKeyword;
+import eu.eexcess.dataformats.userprofile.SecureUserProfile;
+import eu.eexcess.partnerrecommender.api.PartnerConfigurationCache;
+
+public class PartnerConnectorTest {
+
+    @Test
+    public void queryPartnerNative_queryThreeTimes_expectOneTimeBoostrapping() {
+
+        PartnerConfiguration configuration = PartnerConfigurationCache.CONFIG.getPartnerConfiguration();
+        ContextKeyword schroedinger = new ContextKeyword("Erwin Schrödinger");
+        SecureUserProfile userProfile = new SecureUserProfile();
+        userProfile.getContextKeywords().add(schroedinger);
+        PartnerConnector connector = new PartnerConnector();
+
+        ResultList firstTryResults = null;
+        try {
+            firstTryResults = connector.queryPartnerNative(configuration, userProfile, null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (firstTryResults != null) {
+            System.out.println(firstTryResults);
+            assert (609 == firstTryResults.totalResults);
+
+        }
+    }
+
+}
